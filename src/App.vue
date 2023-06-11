72ebd3450fc75781b9ea786b4e6fd154502cc6c5
6d2131 <6d2131@gmail.com>
2023-06-11 09:31:55 +0900

    vue-practice

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Post from './components/Post.vue';
+const title = '匿名掲示板';
+</script>
+
+<template>
+<header>
+ <h1>{{ title }}</h1>
+</header>
+<ul>
+  <Post/>
+  <Post/>
+  <Post/>
+</ul>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,10 @@
+<script setup>
+const content = 'おはよう';
+</script>
+
+<template>
+<p>{{ content }}</p>
+</template>
+
+<style scoped>
+</style>
